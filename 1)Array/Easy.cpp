ba165e5d6a0dddb14b3a0734bc2ff56cbab5986e
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2021-10-07 16:17:53 +0530

    First

--- 1)Array/Easy.cpp
+++ 1)Array/Easy.cpp
@@ -360,3 +360,66 @@ public:
         return false;
     }
 };
+
+12a) Climbing Stairs(1 or 2 steps)
+
+TC-> O(N)
+SC->O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(2);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+            dp[i%2]=dp[0]+dp[1];//dp[i]=dp[i-1]+dp[i-2] SC->O(N)
+        return dp[n%2];
+    }
+};
+
+12b) Climbing Stairs (1 or 3 or 5 steps)
+
+dp[i]=dp[i-1]+dp[i-3]+dp[i-5];
+
+Generalize for any number of steps at a time
+
+TC-> O(N*M) //where M is the size of steps array
+SC->O(N)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            int count=0;
+            for(int j:steps)
+            {
+                if(i-j>=0)
+                count+=dp[i-j];
+            }
+            dp[i]=count;
+        }
+        return dp[n];
+    }
+}; 
+
+13) Fibbonaci Series
+
+TC->O(N)
+SC->O(1)
+
+class Solution {
+public:
+    int fib(int n) {
+        int dp[2];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+            dp[i%2]=dp[0]+dp[1];
+        return dp[n%2];
+    }
+};
