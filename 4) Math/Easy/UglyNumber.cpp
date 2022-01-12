b92712e9af569608a3e60e3806cb114f4f02b8d3
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2022-01-12 23:53:54 +0530

    Day 3

--- 4) Math/Easy/UglyNumber.cpp
+++ 4) Math/Easy/UglyNumber.cpp
@@ -18,12 +18,13 @@ public:
     bool isUgly(int n) {
         if(n<=0)
             return false;  
-        while(n%2==0)
-            n/=2;
+        while(n%5==0)  //divide by 5 first,reduces more time
+            n/=5;
         while(n%3==0)
             n/=3;
-        while(n%5==0)
-            n/=5;
+        while(n%2==0)
+            n/=2;
+        
         return (n==1);
         // if(n%2==0)
         //     n=n/2;
