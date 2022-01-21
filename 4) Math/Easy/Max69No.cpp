3f0150d6523ca21ebd93d4c02c35c39b79e44ccb
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2022-01-21 22:57:34 +0530

    Day12

--- 4) Math/Easy/Max69No.cpp
+++ 4) Math/Easy/Max69No.cpp
@@ -30,3 +30,17 @@ public:
         return num;
     }
 };
+
+b) class Solution {
+public:
+    int maximum69Number (int num) {
+        string s_num = to_string(num);
+        for (auto &c : s_num) {
+            if (c == '6') {
+                c = '9';
+                break;
+            }
+        }
+        return stoi(s_num);
+    }
+};
