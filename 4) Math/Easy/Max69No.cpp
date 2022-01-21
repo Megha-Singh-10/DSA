3afe25476b824e48e50c719db7136bbe68cc07fe
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2022-01-21 22:56:03 +0530

    Day12

---
+++ 4) Math/Easy/Max69No.cpp
@@ -0,0 +1,32 @@
+1323. Maximum 69 Number
+/*You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).*/
+
+TC->O(N)
+SC->O(1)
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int rem=num,q=num,div=pow(10,(int)log10(rem));
+        
+        q=rem/div;  
+        rem=rem%div;
+        cout<<"p "<<rem<<" k "<<q<<"div "<<div<<endl;
+        while(q!=6 && rem!=0)
+        {
+            // if(p!=0)
+            div=pow(10,(int)log10(rem));
+           
+            q=rem/div;
+             rem=rem%div;
+            cout<<"p "<<rem<<" k "<<q<<"div "<<div<<endl;
+        }
+        if(q==6)
+        num+=3*div;
+        else
+            return num;
+        return num;
+    }
+};
