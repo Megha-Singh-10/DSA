e659e8dd19996cbf32165813203145627a8144f3
Megha-Singh-10 <megha.17.becs@acharya.ac.in>
2022-01-23 23:46:15 +0530

    Day 14

--- 2) String/Easy/Jewels&Stones.cpp
+++ 2) String/Easy/Jewels&Stones.cpp
@@ -19,3 +19,31 @@ public:
     }
 };
 
+b) TC->O(N^2) //count->O(N)
+SC->O(1)
+int numJewelsInStones(string jewels, string stones) { 
+    int num = 0;
+    for (int i = 0; i < jewels.size(); i++){
+        num += count(stones.begin(), stones.end(), jewels[i]);
+    }
+    return num;
+}
+
+
+c) /*Explanation
+read J and build jewels hash set.
+read S and count jewels.
+Complexity
+I used hash set and it's O(1) time to check if it contains an element.
+So the total time complexity will be O(J+S), instead of O(JS)
+Space is O(J) */
+
+ int numJewelsInStones(string J, string S) {
+        int res = 0;
+        unordered_set<char> setJ(J.begin(), J.end());
+        for (char s : S)
+            if (setJ.count(s)) res++;
+        return res;
+    }
+
+    
